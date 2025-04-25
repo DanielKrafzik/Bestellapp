1f0673d553172d7867fd58a5f75853c747926860
danielkrafzik <daniel.krafzik@hotmail.de>
2025-04-25 11:06:15 +0200

    db.js data renewed

---
+++ scripts/db.js
@@ -0,0 +1,65 @@
+let myDishes = {
+    "greeceFood": [
+        {
+            "name": "Lukullas Auflauf",
+            "ingredients": "Kartoffeln, Paprika, Champignons und Kochschinken",
+            "price": 12.00
+        },
+        {
+            "name": "Rhodos Grillteller",
+            "ingredients": "2 Schweinerücken-Steaks, 1 Suzuki, 1 Leber, Reis, Salat, Tzatziki",
+            "price": 17.90
+        },
+        {
+            "name": "Akropolis Grillteller",
+            "ingredients": "Gyros, 1 Lamkottlet, 1 Suzuki, 1 Suflaki-Spieß, Reis, Salat, Tzatziki",
+            "price": 18.90
+        },
+    ],
+    "kebap": [
+        {
+            "name": "Falafel Tasche",
+            "ingredients": "4 Falafel, Sauce und Salat",
+            "price": 7.50
+        },
+        {
+            "name": "Kebab Tache",
+            "ingredients": "hausgemachtes Fladenbrot, Kebabfleisch, Sauce und Salat",
+            "price": 8.00
+        },
+        {
+            "name": "Kebab Teller",
+            "ingredients": "Kebabfleisch, Pommes, Sauce und Salat",
+            "price": 14.50
+        }
+    ],
+    "pizza": [
+        {
+            "name": "Pizza Napoletana",
+            "ingredients": "Sardellen und Oliven",
+            "price": {
+                "20cm": 7.00,
+                "26cm": 8.50,
+                "32cm": 10.00
+            }
+        },
+        {
+            "name": "Pizza Funghi",
+            "ingredients": "Champignons",
+            "price": {
+                "20cm": 7.00,
+                "26cm": 8.50,
+                "32cm": 10.00
+            }
+        },
+        {
+            "name": "Pizza Gyros-Schafskäse",
+            "ingredients": "Gyros und Schafskäse",
+            "price": {
+                "20cm": 8.00,
+                "26cm": 10.00,
+                "32cm": 12.50
+            }
+        },
+    ]
+}
