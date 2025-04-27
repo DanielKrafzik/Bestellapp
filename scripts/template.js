80cf903a8953160b0f10b35b5dd575398fb70ce5
Daniel Krafzik <daniel.krafzik@hotmail.de>
2025-04-27 13:11:05 +0200

    renewed index basic Structure

---
+++ scripts/template.js
@@ -0,0 +1,11 @@
+const foodSection = `
+                    <div>
+                        <img>
+                        <h2></h2>
+                        <div></div>
+                    </div>
+`;
+
+const foodDish = `
+                 <h2></h2>
+`
